62d9a1263bf29bddeeab10820f4c34e431b3c5c9
CyrilleFranchet <cf.poubelle@gmail.com>
2019-05-23 15:44:53 +0200

    Revert "Added a control because Splunk launches the command many times"
    This reverts commit b30f19dc

--- bin/xl2.py
+++ bin/xl2.py
@@ -55,6 +55,9 @@ class XL2Command(ReportingCommand):
         return records
 
     def reduce(self, events):
+
+        #splunk_home = os.environ['SPLUNK_HOME']
+
         if self.dtr:
             filename = self.dtr + '_' + datetime.now().strftime('%Y%m%d%H%M%S')
         else:
@@ -102,21 +105,16 @@ class XL2Command(ReportingCommand):
                 fd.flush()
                 yield row
 
-        if events:
-            # Delete any previous ZIP file
-            if os.path.exists(zip_filename):
-                os.remove(zip_filename)
+        # ZIP the file
+        with ZipFile(zip_filename, mode='w', compression=compression) as zip_file:
+            zip_file.write(csv_filename, arcname='sirc-%s.csv' % filename)
 
-            # ZIP the file
-            with ZipFile(zip_filename, mode='w', compression=compression) as zip_file:
-                zip_file.write(csv_filename, arcname='sirc-%s.csv' % filename)
+        # Give RW to the UNIX group
+        os.chmod(zip_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
 
-            # Give RW to the UNIX group
-            os.chmod(zip_filename, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
-
-            # Delete the CSV file
-            if os.path.exists(csv_filename):
-                os.remove(csv_filename)
+        # Delete the CSV file
+        if os.path.exists(csv_filename):
+            os.remove(csv_filename)
 
 
 dispatch(XL2Command, sys.argv, sys.stdin, sys.stdout, __name__)
